49e211b1a7f88093c5c3d2c7b67c4c12e8c51b60
martin-rammerstorfer <rammerstorfer.m@students.htl-leonding.ac.at>
2025-04-04 10:38:53 +0200

    added first character

--- project/js/characters.js
+++ project/js/characters.js
@@ -1,1 +1,27 @@
-let characters = [];
+let characters = [
+  {
+    id: 0,
+    name: "James T. Kirk",
+    firstNames: "James Tiberius",
+    lastName: "Kirk",
+    altNames: [
+      "Jim"
+    ],
+    species: [
+      "Human"
+    ],
+    affiliation: "Federation Starfleet",
+    rank: "Captain",
+    occupation: "Starfleet Officer",
+    serialNumber: "SC937-0176CEC",
+    status: "deceased",
+    birthDate: "2233-03-22",
+    birthPlace: "Earth,United States,Iowa,Riverside",
+    deathDate: "2371",
+    deathPlace: "Veridian III",
+    actors: [
+      "William Shatner",
+      "Paul Wesley"
+    ]
+  }
+];
