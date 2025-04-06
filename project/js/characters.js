37503e0378e8d769ef7fe75ba8f67b357644d404
martin-rammerstorfer <rammerstorfer.m@students.htl-leonding.ac.at>
2025-04-06 21:37:50 +0200

    added ships.js + extended characters.js

--- project/js/characters.js
+++ project/js/characters.js
@@ -1,7 +1,59 @@
+/// <reference path="ships.js" />
+
 let characters = [
   {
     id: 0,
+    name: "Christopher Pike",
+    description: "",
+    firstNames: "Christopher",
+    lastName: "Pike",
+    altNames: [],
+    species: [
+      "Human"
+    ],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "Federation Starfleet",
+    rank: "Fleet captain",
+    occupation: "Starfleet officer",
+    serialNumber: "TG009-00995501",
+    ships: [
+      ships.ANTARES,
+      ships.CHATELET,
+      ships.ARYABHATTA,
+      ships.ENTERPRISE,
+      ships.ENTERPRISE_A,
+      ships.ENTERPRISE_B
+    ],
+    status: "active",
+    birthDate: null,
+    birthPlace: "Earth,United States of America,California,Mojave",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: null,
+    father: "Charlie Pike",
+    mother: "Willa McKinnies",
+    siblings: [],
+    partners: [],
+    actors: [
+      "Jeffrey Hunter",
+      "Sean Kenney",
+      "Anson Mount"
+    ],
+    media: [
+      "TC",
+      "TOS",
+      "ST09",
+      "STID",
+      "DIS",
+      "SNW"
+    ],
+    images: []
+  },
+  {
+    id: 100,
     name: "James T. Kirk",
+    description: "",
     firstNames: "James Tiberius",
     lastName: "Kirk",
     altNames: [
@@ -10,18 +62,56 @@ let characters = [
     species: [
       "Human"
     ],
+    gender: "male",
+    homePlanet: "Earth",
     affiliation: "Federation Starfleet",
     rank: "Captain",
-    occupation: "Starfleet Officer",
+    occupation: "Starfleet officer",
     serialNumber: "SC937-0176CEC",
+    ships: [
+      ships.FARRAGUT,
+      ships.ENTERPRISE,
+      ships.ENTERPRISE_A,
+      ships.ENTERPRISE_B
+    ],
     status: "deceased",
     birthDate: "2233-03-22",
-    birthPlace: "Earth,United States,Iowa,Riverside",
+    birthPlace: "Earth,United States of America,Iowa,Riverside",
     deathDate: "2371",
     deathPlace: "Veridian III",
+    maritalStatus: "widower",
+    father: "George Kirk",
+    mother: "Winona Kirk",
+    siblings: [
+      "George Samuel Kirk"
+    ],
+    partners: [
+      "Miramanee"
+    ],
     actors: [
       "William Shatner",
       "Paul Wesley"
+    ],
+    media: [
+      "TOS",
+      "TAS",
+      "TMP",
+      "TWOK",
+      "TSFS",
+      "TVH",
+      "TFF",
+      "TUC",
+      "GEN",
+      "ST09",
+      "STID",
+      "STB"
+    ],
+    images: [
+      {
+        path: "kirk0.jpg",
+        year: 2266,
+        media: "TOS"
+      }
     ]
   }
 ];
---
+++ project/js/ships.js
@@ -0,0 +1,98 @@
+let ships = {
+  ENTERPRISE: {
+    name: "USS Enterprise",
+    registry: "NCC-1701",
+    class: "Constitution II",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "destroyed"
+  },
+  ENTERPRISE_A: {
+    name: "USS Enterprise A",
+    registry: "NCC-1701-A",
+    class: "Constitution II",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "preserved"
+  },
+  ENTERPRISE_B: {
+    name: "USS Enterprise B",
+    registry: "NCC-1701-B",
+    class: "Excelsior",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "active"
+  },
+  ENTERPRISE_C: {
+    name: "USS Enterprise C",
+    registry: "NCC-1701-C",
+    class: "Ambassador",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "destroyed"
+  },
+  ENTERPRISE_D: {
+    name: "USS Enterprise D",
+    registry: "NCC-1701-D",
+    class: "Galaxy",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "preserved"
+  },
+  ENTERPRISE_E: {
+    name: "USS Enterprise E",
+    registry: "NCC-1701-E",
+    class: "Sovereign",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "out of service"
+  },
+  ENTERPRISE_F: {
+    name: "USS Enterprise F",
+    registry: "NCC-1701-F",
+    class: "Odyssey",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "decommissioned"
+  },
+  ENTERPRISE_G: {
+    name: "USS Enterprise G",
+    registry: "NCC-1701-G",
+    class: "Constitution III",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "active"
+  },
+  FARRAGUT: {
+    name: "USS Farragut",
+    registry: "NCC-1647",
+    class: "Bellerophon",
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "active"
+  },
+  ANTARES: {
+    name: "USS Antares",
+    registry: null,
+    class: null,
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "unknown"
+  },
+  CHATELET: {
+    name: "USS Chatelet",
+    registry: null,
+    class: null,
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "unknown"
+  },
+  ARYABHATTA: {
+    name: "USS Aryabhatta",
+    registry: null,
+    class: null,
+    owner: "United Federation of Planets",
+    operator: "Starfleet",
+    status: "unknown"
+  }
+}
