b511d55931e5c313566d2c53c60d99ea72359eb6
martin-rammerstorfer <rammerstorfer.m@students.htl-leonding.ac.at>
2025-04-10 05:45:24 +0200

    added gsap

---
+++ project/js/script_media.js
@@ -0,0 +1,19 @@
+/// <reference path="ships.js" />
+/// <reference path="media.js" />
+/// <reference path="characters.js" />
+
+gsap.registerPlugin(ScrollTrigger);
+function setAnimation(elem, startX, duration) {
+  gsap.set(elem, {
+    x: startX
+  });
+  gsap.to(elem, {
+    x: 0,
+    duration: duration,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: elem,
+      start: '50% 80%'
+    }
+  });
+}
