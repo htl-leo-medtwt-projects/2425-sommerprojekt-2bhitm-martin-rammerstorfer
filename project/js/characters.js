12f3a8ba236d4b3da656a6429d07c1fbb870f6c7
martin-rammerstorfer <rammerstorfer.m@students.htl-leonding.ac.at>
2025-04-20 22:00:00 +0200

    added ds9, ent, dis characters

--- project/js/characters.js
+++ project/js/characters.js
@@ -320,7 +320,7 @@ let charactersArr = [
     ],
     images: [
       {
-        path: "scott_0.jpg",
+        path: "scotty_0.jpg",
         year: 2266,
         media: media.TOS,
         actor: "James Doohan"
@@ -329,6 +329,63 @@ let charactersArr = [
   },
   {
     id: 4,
+    name: "Hikaru Sulu",
+    description: "Helmsman aboard the USS Enterprise",
+    firstNames: "Hikaru",
+    lastName: "Sulu",
+    titles: null,
+    altNames: [],
+    species: [
+      "Human"
+    ],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "Federation Starfleet",
+    rank: "Lieutenant",
+    occupation: "Helmsman",
+    serialNumber: "SL-0000",
+    ships: [
+      ships.ENTERPRISE,
+      ships.ENTERPRISE_A
+    ],
+    status: "active",
+    birthDate: "2230-04-10",
+    birthPlace: "Earth,United States of America,California,San Francisco",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "George Takei"
+    ],
+    media: [
+      media.TOS,
+      media.TAS,
+      media.TMP,
+      media.TWOK,
+      media.TSFS,
+      media.TVH,
+      media.TFF,
+      media.TUC,
+      media.GEN,
+      media.ST09,
+      media.STID,
+      media.STB
+    ],
+    images: [
+      {
+        path: "sulu_0.jpg",
+        year: 2266,
+        media: media.TOS,
+        actor: "George Takei"
+      }
+    ]
+  },
+  {
+    id: 5,
     name: "Nyota Uhura",
     description: "Communications officer aboard the USS Enterprise",
     firstNames: "Nyota",
@@ -684,6 +741,632 @@ let charactersArr = [
         year: 2366,
         media: media.TNG,
         actor: "Michael Dorn"
+      }
+    ]
+  },
+  {
+    id: 200,
+    name: "Benjamin Sisko",
+    description: "Commander and later Captain of Deep Space Nine",
+    firstNames: "Benjamin",
+    lastName: "Sisko",
+    titles: "Captain",
+    altNames: [],
+    species: ["Human"],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "Federation Starfleet",
+    rank: "Captain",
+    occupation: "Starfleet officer",
+    serialNumber: "DS9-001",
+    ships: [
+      ships.DEEP_SPACE_9
+    ],
+    status: "active",
+    birthDate: "2327-12-12",
+    birthPlace: "Earth",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "widowed",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Avery Brooks"
+    ],
+    media: [
+      media.DS9,
+      media.DIS
+    ],
+    images: [
+      {
+        path: "sisko_0.jpg",
+        year: 2369,
+        media: media.DS9,
+        actor: "Avery Brooks"
+      }
+    ]
+  },
+  {
+    id: 201,
+    name: "Kira Nerys",
+    description: "Former Bajoran resistance fighter and station commander on Deep Space Nine",
+    firstNames: "Kira",
+    lastName: "Nerys",
+    titles: null,
+    altNames: [],
+    species: ["Bajoran"],
+    gender: "female",
+    homePlanet: "Bajor",
+    affiliation: "Bajoran Militia / Federation",
+    rank: "Colonel",
+    occupation: "Military officer",
+    serialNumber: null,
+    ships: [
+      ships.DEEP_SPACE_9
+    ],
+    status: "active",
+    birthDate: "2333-05-08",
+    birthPlace: "Bajor",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Nana Visitor"
+    ],
+    media: [
+      media.DS9,
+      media.DIS
+    ],
+    images: [
+      {
+        path: "kira_0.jpg",
+        year: 2369,
+        media: media.DS9,
+        actor: "Nana Visitor"
+      }
+    ]
+  },
+  {
+    id: 202,
+    name: "Odo",
+    description: "Shape-shifting security officer aboard Deep Space Nine",
+    firstNames: "Odo",
+    lastName: null,
+    titles: null,
+    altNames: [],
+    species: ["Changeling"],
+    gender: "male",
+    homePlanet: "Unknown",
+    affiliation: "Deep Space Nine Security",
+    rank: "Chief of Security",
+    occupation: "Security Officer",
+    serialNumber: null,
+    ships: [
+      ships.DEEP_SPACE_9
+    ],
+    status: "active",
+    birthDate: null,
+    birthPlace: "The Great Link",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: null,
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "René Auberjonois"
+    ],
+    media: [
+      media.DS9,
+      media.DIS
+    ],
+    images: [
+      {
+        path: "odo_0.jpg",
+        year: 2369,
+        media: media.DS9,
+        actor: "René Auberjonois"
+      }
+    ]
+  },
+  {
+    id: 203,
+    name: "Quark",
+    description: "Ferengi bartender and owner of a bar on Deep Space Nine",
+    firstNames: "Quark",
+    lastName: null,
+    titles: null,
+    altNames: [],
+    species: ["Ferengi"],
+    gender: "male",
+    homePlanet: "Ferenginar",
+    affiliation: "None",
+    rank: null,
+    occupation: "Bar owner / Merchant",
+    serialNumber: null,
+    ships: [],
+    status: "active",
+    birthDate: null,
+    birthPlace: "Ferenginar",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Armin Shimerman"
+    ],
+    media: [
+      media.DS9,
+      media.DIS
+    ],
+    images: [
+      {
+        path: "quark_0.jpg",
+        year: 2369,
+        media: media.DS9,
+        actor: "Armin Shimerman"
+      }
+    ]
+  },
+  {
+    id: 204,
+    name: "Julian Bashir",
+    description: "Chief Medical Officer aboard Deep Space Nine",
+    firstNames: "Julian",
+    lastName: "Bashir",
+    titles: "Doctor",
+    altNames: [],
+    species: ["Human"],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "Federation Starfleet",
+    rank: "Commander",
+    occupation: "Doctor",
+    serialNumber: null,
+    ships: [
+      ships.DEEP_SPACE_9
+    ],
+    status: "active",
+    birthDate: "2336-04-15",
+    birthPlace: "Earth",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Alexander Siddig"
+    ],
+    media: [
+      media.DS9,
+      media.DIS
+    ],
+    images: [
+      {
+        path: "bashir_0.jpg",
+        year: 2369,
+        media: media.DS9,
+        actor: "Alexander Siddig"
+      }
+    ]
+  },
+  {
+    id: 300,
+    name: "Jonathan Archer",
+    description: "Captain of the NX-01 Enterprise",
+    firstNames: "Jonathan",
+    lastName: "Archer",
+    titles: "Captain",
+    altNames: [],
+    species: ["Human"],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "United Earth Starfleet",
+    rank: "Captain",
+    occupation: "Starfleet Captain",
+    serialNumber: null,
+    ships: [
+      ships.ENTERPRISE
+    ],
+    status: "active",
+    birthDate: "2112-07-12",
+    birthPlace: "Earth,United States,California,San Francisco",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Scott Bakula"
+    ],
+    media: [
+      media.ENT
+    ],
+    images: [
+      {
+        path: "archer_0.jpg",
+        year: 2151,
+        media: media.ENT,
+        actor: "Scott Bakula"
+      }
+    ]
+  },
+  {
+    id: 301,
+    name: "T'Pol",
+    description: "Science officer aboard the NX-01 Enterprise",
+    firstNames: "T'Pol",
+    lastName: null,
+    titles: null,
+    altNames: [],
+    species: ["Vulcan"],
+    gender: "female",
+    homePlanet: "Vulcan",
+    affiliation: "United Earth Starfleet / Vulcan",
+    rank: "Commander",
+    occupation: "Science Officer",
+    serialNumber: null,
+    ships: [
+      ships.ENTERPRISE
+    ],
+    status: "active",
+    birthDate: "2100-01-01",
+    birthPlace: "Vulcan",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Jolene Blalock"
+    ],
+    media: [
+      media.ENT
+    ],
+    images: [
+      {
+        path: "tpol_0.jpg",
+        year: 2151,
+        media: media.ENT,
+        actor: "Jolene Blalock"
+      }
+    ]
+  },
+  {
+    id: 302,
+    name: "Charles 'Trip' Tucker III",
+    description: "Chief Engineer aboard the NX-01 Enterprise",
+    firstNames: "Charles",
+    lastName: "Tucker",
+    titles: null,
+    altNames: [
+      "Trip"
+    ],
+    species: ["Human"],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "United Earth Starfleet",
+    rank: "Lieutenant Commander",
+    occupation: "Chief Engineer",
+    serialNumber: null,
+    ships: [
+      ships.ENTERPRISE
+    ],
+    status: "active",
+    birthDate: "2115-05-18",
+    birthPlace: "Earth,United States,Texas,Houston",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "married",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Connor Trinneer"
+    ],
+    media: [
+      media.ENT
+    ],
+    images: [
+      {
+        path: "tucker_0.jpg",
+        year: 2151,
+        media: media.ENT,
+        actor: "Connor Trinneer"
+      }
+    ]
+  },
+  {
+    id: 303,
+    name: "Malcolm Reed",
+    description: "Armory officer aboard the NX-01 Enterprise",
+    firstNames: "Malcolm",
+    lastName: "Reed",
+    titles: null,
+    altNames: [],
+    species: ["Human"],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "United Earth Starfleet",
+    rank: "Lieutenant",
+    occupation: "Armory Officer",
+    serialNumber: null,
+    ships: [
+      ships.ENTERPRISE
+    ],
+    status: "active",
+    birthDate: "2110-02-20",
+    birthPlace: "Earth,United States,Virginia,Richmond",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Anthony Montgomery"
+    ],
+    media: [
+      media.ENT
+    ],
+    images: [
+      {
+        path: "reed_0.jpg",
+        year: 2151,
+        media: media.ENT,
+        actor: "Anthony Montgomery"
+      }
+    ]
+  },
+  {
+    id: 304,
+    name: "Hoshi Sato",
+    description: "Communications officer aboard the NX-01 Enterprise",
+    firstNames: "Hoshi",
+    lastName: "Sato",
+    titles: null,
+    altNames: [],
+    species: ["Human"],
+    gender: "female",
+    homePlanet: "Earth",
+    affiliation: "United Earth Starfleet",
+    rank: "Ensign",
+    occupation: "Communications Officer",
+    serialNumber: null,
+    ships: [
+      ships.ENTERPRISE
+    ],
+    status: "active",
+    birthDate: "2117-11-09",
+    birthPlace: "Earth,United States,California,Los Angeles",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "Linda Park"
+    ],
+    media: [
+      media.ENT
+    ],
+    images: [
+      {
+        path: "sato_0.jpg",
+        year: 2151,
+        media: media.ENT,
+        actor: "Linda Park"
+      }
+    ]
+  },
+  {
+    id: 305,
+    name: "Dr. Phlox",
+    description: "Chief Medical Officer aboard the NX-01 Enterprise",
+    firstNames: "Phlox",
+    lastName: null,
+    titles: "Doctor",
+    altNames: [],
+    species: ["Denobulan"],
+    gender: "male",
+    homePlanet: "Denobula",
+    affiliation: "United Earth Starfleet",
+    rank: "Commander",
+    occupation: "Chief Medical Officer",
+    serialNumber: null,
+    ships: [
+      ships.ENTERPRISE_NX1
+    ],
+    status: "active",
+    birthDate: null,
+    birthPlace: "Denobula",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "married",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: [
+      "John Billingsley"
+    ],
+    media: [
+      media.ENT
+    ],
+    images: [
+      {
+        path: "phlox_0.jpg",
+        year: 2151,
+        media: media.ENT,
+        actor: "John Billingsley"
+      }
+    ]
+  },
+  {
+    id: 400,
+    name: "Michael Burnham",
+    description: "Science specialist and former Starfleet officer aboard the USS Discovery",
+    firstNames: "Michael",
+    lastName: "Burnham",
+    titles: "Commander",
+    altNames: ["Burnham"],
+    species: ["Human"],
+    gender: "female",
+    homePlanet: "Earth",
+    affiliation: "Federation Starfleet",
+    rank: "Commander",
+    occupation: "Science Specialist",
+    serialNumber: null,
+    ships: [ships.DISCOVERY],
+    status: "active",
+    birthDate: null,
+    birthPlace: "Earth",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: ["Sonequa Martin-Green"],
+    media: [media.DIS],
+    images: [
+      {
+        path: "burnham_0.jpg",
+        year: 2255,
+        media: media.DIS,
+        actor: "Sonequa Martin-Green"
+      }
+    ]
+  },
+  {
+    id: 401,
+    name: "Saru",
+    description: "First officer aboard the USS Discovery and a pioneering Kelpien",
+    firstNames: "Saru",
+    lastName: null,
+    titles: "Lieutenant Commander",
+    altNames: [],
+    species: ["Kelpien"],
+    gender: "male",
+    homePlanet: "Kaminar",
+    affiliation: "Federation Starfleet",
+    rank: "Lieutenant Commander",
+    occupation: "First Officer",
+    serialNumber: null,
+    ships: [ships.DISCOVERY],
+    status: "active",
+    birthDate: null,
+    birthPlace: "Kaminar",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: ["Doug Jones"],
+    media: [media.DIS],
+    images: [
+      {
+        path: "saru_0.jpg",
+        year: 2255,
+        media: media.DIS,
+        actor: "Doug Jones"
+      }
+    ]
+  },
+  {
+    id: 402,
+    name: "Paul Stamets",
+    description: "Chief engineer aboard the USS Discovery and a brilliant astromycologist",
+    firstNames: "Paul",
+    lastName: "Stamets",
+    titles: null,
+    altNames: [],
+    species: ["Human"],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "Federation Starfleet",
+    rank: "Lieutenant Commander",
+    occupation: "Engineer / Astromycologist",
+    serialNumber: null,
+    ships: [ships.DISCOVERY],
+    status: "active",
+    birthDate: null,
+    birthPlace: "Unknown",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: ["Anthony Rapp"],
+    media: [media.DIS],
+    images: [
+      {
+        path: "stamets_0.jpg",
+        year: 2255,
+        media: media.DIS,
+        actor: "Anthony Rapp"
+      }
+    ]
+  },
+  {
+    id: 403,
+    name: "Ash Tyler",
+    description: "Security officer aboard the USS Discovery with a mysterious past",
+    firstNames: "Ash",
+    lastName: "Tyler",
+    titles: null,
+    altNames: [],
+    species: ["Human"],
+    gender: "male",
+    homePlanet: "Earth",
+    affiliation: "Federation Starfleet",
+    rank: "Lieutenant",
+    occupation: "Security Officer",
+    serialNumber: null,
+    ships: [ships.DISCOVERY],
+    status: "active",
+    birthDate: null,
+    birthPlace: "Unknown",
+    deathDate: null,
+    deathPlace: null,
+    maritalStatus: "single",
+    father: null,
+    mother: null,
+    siblings: [],
+    partners: [],
+    actors: ["Shazad Latif"],
+    media: [media.DIS],
+    images: [
+      {
+        path: "tyler_0.jpg",
+        year: 2255,
+        media: media.DIS,
+        actor: "Shazad Latif"
       }
     ]
   }
